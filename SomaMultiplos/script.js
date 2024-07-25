f2dcdbb2cbb9a62cbf8c78a245ce0dc6f98c8e9c
Julia Hang <153762550+JuliaHang2@users.noreply.github.com>
2024-07-25 16:56:24 -0300

    Add files via upload

---
+++ SomaMultiplos/script.js
@@ -0,0 +1,13 @@
+document.getElementById("botao").addEventListener("click", media);
+function media() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let num3 = document.getElementById("num3").value;
+
+    let media1 = num1 + num2 + num3;
+    let media = media1/3;
+
+    document.getElementById("ResultMedia").value = media;
+}
+  
+
