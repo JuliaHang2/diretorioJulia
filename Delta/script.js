50eb9af77b06a7bfe11c6503b6fdb80a4bf07e6a
Julia Hang <153762550+JuliaHang2@users.noreply.github.com>
2024-07-31 15:12:18 -0300

    script.js

--- Delta/script.js
+++ Delta/script.js
@@ -3,5 +3,23 @@ function delta() {
     let num1 = document.getElementById("a").value;
     let num2 = document.getElementById("b").value;
     let num3 = document.getElementById("c").value;
-    document.getElementById("resultado").innerHTML=(num2^2)-(4*num1*num3); 
-}
+
+    const Delta = (num2*num2)-(4*num1*num3);
+
+    const resolucao = `
+
+    Δ = b² - 4ac<br>
+
+    Δ = ${num2}² - 4 * ${num1} * ${num3}<br>
+
+    Δ = ${num2 * num2} - ${4 * num1} * ${num3}<br>
+
+    Δ = ${num2 * num2} - (${4 * num1 * num3})<br>
+
+    Δ = ${Delta}
+
+            `;
+    document.getElementById('resultado').innerHTML = resolucao;
+        } 
+
+        
